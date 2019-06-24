603a9d6614d74ac24a423b35aa751884d01e24eb
zwol3027 <zwol3027@uni.sydney.edu.au>
2019-06-24 14:52:21 +0530

    test printing labels

--- classifier_utils.py
+++ classifier_utils.py
@@ -126,7 +126,8 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
   assert len(segment_ids) == max_seq_length
 
   if label_list is not None:
-    print("LABEL_info", example.label, label_map[example.label])
+    print("LABEL_info", example.label)
+    print("LABEL_map", label_map[example.label])
     label_id = label_map[example.label]
   else:
     label_id = example.label
